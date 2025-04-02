7915769509caec6b7763067967e5c8a60b9b5e07
StanWeerman <stanweerman@gmail.com>
2025-04-02 01:14:06 -0700

    queap_tree tests run from root.zig

--- src/root.zig
+++ src/root.zig
@@ -16,5 +16,20 @@ test "List 1" {
     try testing.expect(ql.head.?.data == 1);
 }
 
+fn lessThan(context: void, a: u8, b: u8) std.math.Order {
+    _ = context;
+    return std.math.order(a, b);
+}
+
+const QTlt = QueapTree(u8, void, lessThan);
+
+test "Tree 1" {
+    var qt = try QTlt.init(testing.allocator, {});
+    try qt.insert(1);
+    try qt.insert(2);
+    try qt.insert(3);
+    defer qt.deinit();
+}
+
 // Fuzz testing: Randomly insert into queap, then take mins one by one
 // Save deleted mins, if any newly deleted is lower, error
