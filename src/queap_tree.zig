83732ea89d41e70b32fa4db08b0dc833a98b58c2
StanWeerman <stanweerman@gmail.com>
2025-04-03 12:48:15 -0700

    Uncomment out test

--- src/queap_tree.zig
+++ src/queap_tree.zig
@@ -415,34 +415,34 @@ test "Root Maintains Min" {
     try testing.expect(1 == qt.root.p.?.data.value);
 }
 
-// test "Fuzz Testing Add" {
-//     var prng = std.Random.DefaultPrng.init(blk: {
-//         var seed: u64 = undefined;
-//         try std.posix.getrandom(std.mem.asBytes(&seed));
-//         break :blk seed;
-//     });
-//     const rand = prng.random();
+test "Fuzz Testing Add" {
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
 
-//     for (1..100) |t| {
-//         _ = t;
-//         var qt = try QTlt.init(testing.allocator, {});
-//         const max_leaf = qt.root.data.child[0].?;
-//         defer qt.deinit();
+    for (1..100) |t| {
+        _ = t;
+        var qt = try QTlt.init(testing.allocator, {});
+        const max_leaf = qt.root.data.child[0].?;
+        defer qt.deinit();
 
-//         var min: u8 = std.math.maxInt(u8);
-//         for (0..rand.int(u8)) |i| {
-//             _ = i;
-//             const ele = rand.int(u8);
-//             min = @min(min, ele);
-//             try qt.insert(ele);
-//             try testing.expect(min == qt.root.p.?.data.value);
-//             try testing.expect(min == max_leaf.p.?.data.value);
-//             // std.debug.print("New ele: {}\n", .{ele});
-//             try testing.expect(try qt.find_node(ele) != null);
-//         }
-//         // try print_tree(QTlt, &qt);
-//     }
-// }
+        var min: u8 = std.math.maxInt(u8);
+        for (0..rand.int(u8)) |i| {
+            _ = i;
+            const ele = rand.int(u8);
+            min = @min(min, ele);
+            try qt.insert(ele);
+            try testing.expect(min == qt.root.p.?.data.value);
+            try testing.expect(min == max_leaf.p.?.data.value);
+            // std.debug.print("New ele: {}\n", .{ele});
+            try testing.expect(try qt.find_node(ele) != null);
+        }
+        // try print_tree(QTlt, &qt);
+    }
+}
 
 test "Print Tree" {
     var qt = try QTlt.init(testing.allocator, {});
